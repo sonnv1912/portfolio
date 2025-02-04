dcef05fe56edc9e5359aa812047e50414881077e
sonnv <sonnv1912@gmail.com>
2025-02-04 15:59:29 +0700

    fix: import app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import '@config/locales/i18n';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import { App } from './App';
+import { App } from './app';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>

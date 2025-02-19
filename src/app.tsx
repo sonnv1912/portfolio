1701dbc439f7450d7f602853f4d9472e985faf67
sonnv1912 <sonnv1912@gmail.com>
2025-02-19 10:20:05 +0700

    feat: reverse code

---
+++ src/app.tsx
@@ -0,0 +1,53 @@
+import { Navigate, Route, Routes } from 'react-router';
+import { Contact } from './pages/contact';
+import { Degrees } from './pages/degrees';
+import { Me } from './pages/me';
+import { Projects } from './pages/projects';
+import { Skills } from './pages/skills';
+import { Experiences } from './pages/experiences';
+
+export const App = () => {
+	const routes = [
+		{
+			path: '/portfolio/me',
+			component: <Me />,
+		},
+		{
+			path: '/portfolio/skills',
+			component: <Skills />,
+		},
+		{
+			path: '/portfolio/projects',
+			component: <Projects />,
+		},
+		{
+			path: '/portfolio/degrees',
+			component: <Degrees />,
+		},
+		{
+			path: '/portfolio/contact',
+			component: <Contact />,
+		},
+		{
+			path: '/portfolio/experiences',
+			component: <Experiences />,
+		},
+	];
+
+	return (
+		<Routes>
+			{routes.map((route) => (
+				<Route
+					key={route.path}
+					path={route.path}
+					element={route.component}
+				/>
+			))}
+
+			<Route
+				path='/portfolio'
+				element={<Navigate to='/portfolio/me' />}
+			/>
+		</Routes>
+	);
+};

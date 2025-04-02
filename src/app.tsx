e77a42e4329fbb7983b7d0b34c94dea93cdcf8ae
sonnv1912 <sonnv1912@gmail.com>
2025-04-02 15:14:05 +0700

    feat: implement tab management with animation in App component; enhance SidebarItem for active state indication

--- src/app.tsx
+++ src/app.tsx
@@ -6,21 +6,64 @@ import { Skills } from './pages/skills';
 import { Degrees } from './pages/degrees';
 import { Projects } from './pages/projects';
 import { Contact } from './pages/contact';
+import { AnimatePresence, motion } from 'motion/react';
+import { useMemo } from 'react';
 
 export const App = () => {
 	const [tab] = useQueryState('tab');
 
+	const tabs = useMemo(() => {
+		const result = [
+			{
+				show: !tab || tab === 'me',
+				component: <Me />,
+				id: 'me',
+			},
+			{
+				show: tab === 'skills',
+				component: <Skills />,
+				id: 'skills',
+			},
+			{
+				show: tab === 'degrees',
+				component: <Degrees />,
+				id: 'degrees',
+			},
+			{
+				show: tab === 'projects',
+				component: <Projects />,
+				id: 'projects',
+			},
+			{
+				show: tab === 'contact',
+				component: <Contact />,
+				id: 'contact',
+			},
+		];
+
+		return result.filter((t) => t.show);
+	}, [tab]);
+
 	return (
 		<DefaultLayout>
-			{(!tab || tab === 'me') && <Me />}
-
-			{tab === 'skills' && <Skills />}
-
-			{tab === 'degrees' && <Degrees />}
-
-			{tab === 'projects' && <Projects />}
-
-			{tab === 'contact' && <Contact />}
+			<AnimatePresence initial={true}>
+				{tabs.map((t) => (
+					<motion.div
+						key={t.id}
+						transition={{
+							duration: 0.5,
+						}}
+						exit={{ opacity: 0, y: '10%' }}
+						initial={{ opacity: 0, y: '10%' }}
+						animate={{
+							opacity: 1,
+							y: 0,
+						}}
+					>
+						{t.component}
+					</motion.div>
+				))}
+			</AnimatePresence>
 
 			<Tooltip
 				id='tooltip'
--- src/components/sidebar-item.tsx
+++ src/components/sidebar-item.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useQueryState } from 'nuqs';
 import type { IconType } from 'react-icons';
 
 type Props = {
@@ -11,11 +12,16 @@ type Props = {
 };
 
 const SidebarItem = ({ data, onPress }: Props) => {
+	const [tab] = useQueryState('tab');
+
 	return (
 		<div
 			className={clsx(
 				'flex size-12 flex-col items-center justify-center hover:bg-woodsmoke-950',
 				'sidebar-item cursor-pointer select-none gap-1 rounded-md transition-all duration-500',
+				{
+					'bg-woodsmoke-950': tab === data.tab,
+				},
 			)}
 			data-tooltip-id='tooltip'
 			data-tooltip-content={data.label}

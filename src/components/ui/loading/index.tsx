30553752fafb664050aaf5c26d71e075765fc0f7
sonnv1912 <sonnv1912@gmail.com>
2025-04-04 18:07:57 +0700

    refactor: simplify Loading component structure by removing unnecessary fragments

--- src/components/ui/loading/index.tsx
+++ src/components/ui/loading/index.tsx
@@ -11,38 +11,36 @@ export const Loading = ({ loading, size = 60 }: PropsWithChildren<Props>) => {
 	const lineSize = size / 12;
 
 	return (
-		<>
-			{loading && (
+		loading && (
+			<div
+				style={{
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					right: 0,
+					bottom: 0,
+					zIndex: 9999,
+					backgroundColor: '#1b1b1b',
+				}}
+			>
 				<div
-					style={{
-						position: 'fixed',
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						zIndex: 9999,
-						backgroundColor: '#1b1b1b',
-					}}
+					className={clsx(styles.loader)}
+					style={{ width: size, height: size }}
 				>
 					<div
-						className={clsx(styles.loader)}
-						style={{ width: size, height: size }}
-					>
-						<div
-							className={clsx(styles.inner, styles.one)}
-							style={{ borderBottom: `${lineSize}px solid #efeffa` }}
-						/>
-						<div
-							className={clsx(styles.inner, styles.two)}
-							style={{ borderRight: `${lineSize}px solid #efeffa` }}
-						/>
-						<div
-							className={clsx(styles.inner, styles.three)}
-							style={{ borderTop: `${lineSize}px solid #efeffa` }}
-						/>
-					</div>
+						className={clsx(styles.inner, styles.one)}
+						style={{ borderBottom: `${lineSize}px solid #efeffa` }}
+					/>
+					<div
+						className={clsx(styles.inner, styles.two)}
+						style={{ borderRight: `${lineSize}px solid #efeffa` }}
+					/>
+					<div
+						className={clsx(styles.inner, styles.three)}
+						style={{ borderTop: `${lineSize}px solid #efeffa` }}
+					/>
 				</div>
-			)}
-		</>
+			</div>
+		)
 	);
 };

a59be9705bb26f7b4500ac358155daceed2c9199
sonnv1912 <sonnv1912@gmail.com>
2025-04-04 17:57:24 +0700

    refactor: remove AnimatePresence and motion from Loading component; simplify loading structure

--- src/components/ui/loading/index.tsx
+++ src/components/ui/loading/index.tsx
@@ -1,8 +1,7 @@
+import { useMyProfile } from '@hooks/use-my-profile';
 import clsx from 'clsx';
 import { type PropsWithChildren, useEffect, useState } from 'react';
 import styles from './loading.module.css';
-import { AnimatePresence, motion } from 'motion/react';
-import { useMyProfile } from '@hooks/use-my-profile';
 
 type Props = {
 	loading?: boolean;
@@ -34,45 +33,40 @@ export const Loading = ({ loading, size = 60, children }: PropsWithChildren<Prop
 	}, [me.education, me.image]);
 
 	return (
-		<div>
-			<AnimatePresence>
-				{(loading || loadingStyle) && (
-					<motion.div
-						initial={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.8 }}
-						style={{
-							position: 'fixed',
-							top: 0,
-							left: 0,
-							right: 0,
-							bottom: 0,
-							zIndex: 9999,
-							backgroundColor: '#1b1b1b',
-						}}
+		<>
+			{(loading || loadingStyle) && (
+				<div
+					style={{
+						position: 'fixed',
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						zIndex: 9999,
+						backgroundColor: '#1b1b1b',
+					}}
+				>
+					<div
+						className={clsx(styles.loader)}
+						style={{ width: size, height: size }}
 					>
 						<div
-							className={clsx(styles.loader)}
-							style={{ width: size, height: size }}
-						>
-							<div
-								className={clsx(styles.inner, styles.one)}
-								style={{ borderBottom: `${lineSize}px solid #efeffa` }}
-							/>
-							<div
-								className={clsx(styles.inner, styles.two)}
-								style={{ borderRight: `${lineSize}px solid #efeffa` }}
-							/>
-							<div
-								className={clsx(styles.inner, styles.three)}
-								style={{ borderTop: `${lineSize}px solid #efeffa` }}
-							/>
-						</div>
-					</motion.div>
-				)}
-			</AnimatePresence>
+							className={clsx(styles.inner, styles.one)}
+							style={{ borderBottom: `${lineSize}px solid #efeffa` }}
+						/>
+						<div
+							className={clsx(styles.inner, styles.two)}
+							style={{ borderRight: `${lineSize}px solid #efeffa` }}
+						/>
+						<div
+							className={clsx(styles.inner, styles.three)}
+							style={{ borderTop: `${lineSize}px solid #efeffa` }}
+						/>
+					</div>
+				</div>
+			)}
 
 			{children}
-		</div>
+		</>
 	);
 };

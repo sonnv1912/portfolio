5f01a5fa0f4d3226df9c4d034eeef0fe984c802b
sonnv1912 <sonnv1912@gmail.com>
2025-04-07 14:06:58 +0700

    fix: change name

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,11 +1,11 @@
 import { jwtDecode } from 'jwt-decode';
 
-const token = jwtDecode<{ github_token: string }>(
+const test = jwtDecode<{ github_token: string }>(
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnaXRodWJfdG9rZW4iOiJnaHBfZ1ByOFVYckdqMkJlbWxmMDBQOHhmaWFSYXd5eHlEMmtWbzB2IiwiaWF0IjoxNzQ0MDA5NDE4fQ.bFm_-0QxcrJVkWeDqExk75E0WrBrmuQDLWNQohIPYK8',
 );
 
 export const env = {
-	githubToken: token.github_token,
+	githubToken: test.github_token,
 	owner: 'sonnv1912',
 	repo: {
 		stickyClip: 'sticky-clip',

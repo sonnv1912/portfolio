5a8b0bef0197727417d44ac2acddb0720a29433a
sonnv1912 <sonnv1912@gmail.com>
2025-04-07 14:18:30 +0700

    fix: revert githubToken to use environment variable in env.ts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-	githubToken: 'ghp_SjE9aSv8Vq9B4fhS0' + '8mEV3Cgqxublo4Lr8Ur',
+	githubToken: import.meta.env.VITE_GITHUB_KEY,
 	owner: 'sonnv1912',
 	repo: {
 		stickyClip: 'sticky-clip',

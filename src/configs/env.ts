d877bbb79967335f40163b30a34bb668084baf76
sonnv1912 <sonnv1912@gmail.com>
2025-04-07 14:06:00 +0700

    feat: add jwt-decode dependency and update githubToken retrieval in env.ts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,11 @@
+import { jwtDecode } from 'jwt-decode';
+
+const token = jwtDecode<{ github_token: string }>(
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnaXRodWJfdG9rZW4iOiJnaHBfZ1ByOFVYckdqMkJlbWxmMDBQOHhmaWFSYXd5eHlEMmtWbzB2IiwiaWF0IjoxNzQ0MDA5NDE4fQ.bFm_-0QxcrJVkWeDqExk75E0WrBrmuQDLWNQohIPYK8',
+);
+
 export const env = {
-	githubToken: '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',
+	githubToken: token.github_token,
 	owner: 'sonnv1912',
 	repo: {
 		stickyClip: 'sticky-clip',

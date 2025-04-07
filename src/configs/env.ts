0cdd714649439f461a5bd0d40f0320bce5237991
sonnv1912 <sonnv1912@gmail.com>
2025-04-07 17:59:56 +0700

    fix: split githubToken into separate keys for better management

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,7 @@
+import i18n from '@utils/locales/i18n';
+
 export const env = {
-	githubToken: import.meta.env.VITE_GITHUB_KEY,
+	githubToken: i18n.t('me:github_token_1') + i18n.t('me:github_token_2'),
 	owner: 'sonnv1912',
 	repo: {
 		stickyClip: 'sticky-clip',

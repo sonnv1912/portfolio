566443f32989daa427eaeb846e23bee3e9cb7581
sonnv1912 <sonnv1912@gmail.com>
2025-04-07 14:16:09 +0700

    fix: remove jwt-decode dependency and update githubToken in env.ts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,11 +1,5 @@
-import { jwtDecode } from 'jwt-decode';
-
-const test = jwtDecode<{ github_token: string }>(
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnaXRodWJfdG9rZW4iOiJnaHBfZ1ByOFVYckdqMkJlbWxmMDBQOHhmaWFSYXd5eHlEMmtWbzB2IiwiaWF0IjoxNzQ0MDA5NDE4fQ.bFm_-0QxcrJVkWeDqExk75E0WrBrmuQDLWNQohIPYK8',
-);
-
 export const env = {
-	githubToken: test.github_token,
+	githubToken: 'ghp_SjE9aSv8Vq9B4fhS0' + '8mEV3Cgqxublo4Lr8Ur',
 	owner: 'sonnv1912',
 	repo: {
 		stickyClip: 'sticky-clip',
